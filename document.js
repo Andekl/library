2093bab9b6581183fa310d191d266b08ab7effee
Andekl <andreas.eklundit94@gmail.com>
2018-03-08 16:06:18 +0100

    Felhantering för View

--- document.js
+++ document.js
@@ -6,14 +6,20 @@ let authorInput = document.getElementById('authorInput')
 let bookInput = document.getElementById('bookInput')
 let list = document.getElementById('list')
 let requestBtn = document.getElementById('requestBtn')
+let buttonView = document.getElementById('buttonView')
 
-let url ='https://www.forverkliga.se/JavaScript/api/crud.php?requestKey'
-let titleInput = bookInput.value;
-let authorInut = authorInput.value;
+let author = authorInput.value
+let title = bookInput.value
 
-function sendAjax(){
+let url ='https://www.forverkliga.se/JavaScript/api/crud.php'
+let request_qs = '?requestKey'
+
+let key = 'EPAR3'
+
+//Request function key button
+/*function sendAjax(){
   let responseDiv = document.getElementById('response');
-  fetch(url)
+  fetch(url+request_qs)
   .then ( response => {
     console.log('Response from server: ',response)
     return response.text();
@@ -23,27 +29,54 @@ function sendAjax(){
     responseDiv.innerText = text;
   })
 }
-requestBtn.addEventListener('click', sendAjax);
+requestBtn.addEventListener('click', sendAjax);*/
 
+function addTitle_Author(){
+  let author = authorInput.value
+  let title = bookInput.value
+  let add_query = `?op=insert&title=${title}&author=${author}&key=${key}`;
+  fetch(url+add_query)
+  .then ( response => {
+    console.log('added '+title+' '+author)
+    return response.json();
+  })
+}
+let counter = 0;
+function viewTitle_Author(){
+  let author = authorInput.value
+  let title = bookInput.value
+  let responseUl = document.getElementById('list');
+  let view_query = `?op=select&key=${key}`;
+  fetch(url+view_query)
+    .then( response =>{
+      console.log('getting table')
+      return response.json();
+    })
+    .then(obj =>{
+      if(counter === 1){
+        list.innerText = 'request denied, try again'
+        console.log(obj.data);
+        counter = 0;
+      }
+      else if(obj.status === 'success'){
+        console.log(obj);
+        console.log(counter);
+        console.log(obj.data);
 
-/*
-let idCounter = 0;
+        list.innerHTML = obj.data.map( book => `${book.title} by ${book.author} <br></br>` )
+      }
+      else{
+        counter++;
+        console.log(counter);
+         viewTitle_Author();
+       }
+    })
+
+}
+
 //Add author and book to the top of the list
-  buttonAdd.addEventListener('click', function(event){
-    idCounter++;
-    let id = 'id'+idCounter;//Creates a unique valid id
-    const li = document.createElement('li');//Creates new li element
-    const btn = document.createElement("BUTTON");//Creates new button with the element
-    const btnText = document.createTextNode("CLICK ME");//Button text
-    li.innerText ="Author: "+authorInput.value+" Title: "+ bookInput.value
-    let first = document.querySelector('ul :first-child');//Finds first li-element
-    btn.id = id;//Sets unique id to the new button
-    console.log(id);
-    btn.appendChild(btnText);//Sets text to the new button
-    list.insertBefore(li, first);
-    list.insertBefore(btn, first);
-
-  });
+  buttonAdd.addEventListener('click', addTitle_Author);
+  buttonView.addEventListener('click',viewTitle_Author);
 
   //Delete the latest author and book from the list
   buttonDelete.addEventListener('click',event =>{
@@ -52,6 +85,6 @@ let idCounter = 0;
     if(first !== null)
       //list.removeChild(first);
       list.removeChild()
-  });*/
+  });
 
 })
